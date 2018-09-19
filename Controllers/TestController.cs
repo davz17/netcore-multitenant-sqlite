6a17219eb6287524baaacce120e976e2f68d840d
rodel-p-ocfemia <rodel.p.ocfemia@gmail.com>
2018-09-19 16:03:42 +0800

    First commit

---
+++ Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webmarks.xyz.Infrastructure;
+using webmarks.xyz.Models;
+
+namespace webmarks.xyz.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Test")]
+    [ServiceFilter(typeof(TenantAttribute))]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var host = "landing page";
+            if (RouteData.Values["tenant"] != null)
+            {
+                host = (RouteData.Values["tenant"] as Tenants).Host;
+            }
+            return Ok("From TestController API - " + host);
+        }
+    }
+}
---
+++ Migrations/20180918055902_initialcreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace webmarks.xyz.Migrations
+{
+    public partial class initialcreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tenants",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Host = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Style = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tenants", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tenants");
+        }
+    }
+}
